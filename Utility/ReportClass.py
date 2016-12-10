98b98cf3d77fc75eca198d1b7c23384e776f625d
depth <omonait15@gmail.com>
2016-12-10 11:10:45 +0200

    Последняя версия API отчетов

--- Utility/ReportClass.py
+++ Utility/ReportClass.py
@@ -1,17 +1,24 @@
 from PyQt5.QtCore import QDate
 from PyQt5.QtGui import QFont
+from PyQt5.QtGui import QTextCursor
 from PyQt5.QtGui import QTextDocument
+from PyQt5.QtGui import QTextTableFormat
 from PyQt5.QtPrintSupport import QPrintPreviewDialog, QPrinter, QPrintDialog
 from PyQt5.QtSql import QSqlTableModel
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtWidgets import QTextEdit
 
 
 class Report():
 
+    printed = False
     printer = None
     previewDialog = None
     document = None
+    FontSize = 12
     Html = ''
+    webView = None
 
     def __init__(self, name = 'Отчет', date = QDate.currentDate()):
         self.__initObjects()
@@ -19,21 +26,36 @@ class Report():
         self.name = name
         self.date = date
 
+        # self.document.setDefaultStyleSheet("""
+        # table { border: 5}
+        # td { spacing: 10}
+        # """)
+        self.Html += """
+        <style type=\"text/css\">
+        table {
+            border: 5px solid black
+        }
+        </style>
+        """
         self.__setHeader()
 
     def showPreview(self):
+        self.previewDialog = QPrintPreviewDialog(self.printer)
+        self.previewDialog.setMaximumSize(1500,1000)
+        self.previewDialog.paintRequested.connect(self.Print)
         self.previewDialog.exec()
+        self.printed = True
+        # self.Print()
 
     def __initObjects(self):
         self.printer = QPrinter(QPrinter.HighResolution)
         #self.printer.setOutputFormat(QPrinter.PdfFormat)
         self.printer.setPageSize(QPrinter.A4)
 
-        self.previewDialog = QPrintPreviewDialog()
-        #self.previewDialog.paintRequested.connect()
-
         self.document = QTextDocument()
-        self.document.setDefaultFont(QFont("Times",14))
+        self.document.setDefaultFont(QFont("Times",self.FontSize))
+
+        self.webView = QWebEngineView()
 
     def __setHeader(self):
         self.Html += "\
@@ -47,54 +69,60 @@ class Report():
         self.Html += string
 
     def addTable(self,fields, rows):
-        self.Html += "<table  width='100%'>"
+        self.Html += '<table border="2" style=" border-style:solid; border-color:black" cellspacing="0" cellpadding="6" width="100%">'
 
-        self.Html += "<tr  border='1'>"
-        self.Html += "<th><strong>№</strong></th>"
+        self.Html += "<thead><tr>"
+        self.Html += "<th width='8%'><strong>№</strong></th>"
         for item in fields:
-            self.Html += "<th><strong>"+str(item)+"</strong></th>"
+            self.Html += "<th ><strong>"+str(item)+"</strong></th>"
 
-        self.Html += "</tr>"
+        self.Html += "</tr></thead>"
         N = 0
+        self.Html += "<tbody>"
         for row in rows:
             N += 1
             self.Html += "<tr>"
             self.Html += "<td align='center'>"+str(N)+"</td>"
             for item in row:
-                self.Html += "<td align='center'>"+str(item)+"</td>"
+                self.Html += "<td padding='5' align='center'>"+str(item)+"</td>"
             self.Html += "</tr>"
 
 
-        self.Html += "</table>"
+        self.Html += "</tbody></table>"
 
-    def addTableFromModel(self, model, fieldsIndexes = None, fieldsNames = None, fieldsConstraint = None):
+    def addTableFromModel(self, model, fieldsIndexes = [], fieldsNames = {}, delegates = {}):
         fields = []
-        fieldsNames.reverse()
+
         for i in range(0, model.columnCount()):
-            if fieldsIndexes != None:
-                if i in fieldsIndexes:
-                    if fieldsConstraint != None:
-                        fields.append(fieldsConstraint(fieldsNames.pop()))
-                    else:
-                        fields.append(fieldsNames.pop())
+            if len(fieldsIndexes) > 0 and i in fieldsIndexes:
+                    pass
             else:
-                fields.append(model.record(0).fieldName(i))
+                try:
+                    fields.append(fieldsNames[i])
+                except:
+                    fields.append(model.record(0).fieldName(i))
 
         rows = []
         for i in range(0, model.rowCount()):
             row = []
             for j in range(0, model.columnCount()):
-                if j in fieldsIndexes:
-                    row.append(model.record(i).value(j))
+                if len(fieldsIndexes) > 0 and j in fieldsIndexes:
+                        pass
+                else:
+                    try:
+                        row.append(delegates[j](model.record(i).value(j)))
+                    except:
+                        row.append(model.record(i).value(j))
             rows.append(row)
 
         self.addTable( fields, rows)
 
     def Print(self):
-        dialog = QPrintDialog(self.printer)
+        self.printer.setFullPage(True)
 
-        if dialog.exec():
-            self.printer.setFullPage(True)
-            self.document.setHtml(self.Html)
-            self.document.print(self.printer)
+        self.document.setHtml(self.Html)
+
+
+        # if not self.printed:
+        self.document.print(self.printer)
 
